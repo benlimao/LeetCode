7019bcd0292f56261cbea5bfeba92a247c996a0c
benlimao <415639957@qq.com>
2017-08-08 17:46:42 +0800

    Update 198. House Robber.cpp

--- LeetCode/198. House Robber.cpp
+++ LeetCode/198. House Robber.cpp
@@ -1,3 +1,22 @@
+//1.动态规划
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(0 == n)
+            return 0;
+        if(1 == n)
+            return nums[0];
+        vector<int> dp(n, 0);
+        dp[0] = nums[0];
+        dp[1] = nums[0] > nums[1] ? nums[0] : nums[1];
+        for(int i = 2; i < n; i++)
+            dp[i] = (dp[i - 2] + nums[i]) > dp[i - 1] ? (dp[i - 2] + nums[i]) : dp[i - 1];
+        return dp[n - 1];
+    }
+};
+
+//2.节省空间的方法
 class Solution {
 public:
     int rob(vector<int>& nums) {
