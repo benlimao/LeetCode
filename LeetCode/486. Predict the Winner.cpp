c6b985b7a8b78965b2d662c42c5caa75b1987519
benlimao <415639957@qq.com>
2017-08-11 21:21:31 +0800

    Update 486. Predict the Winner.cpp

--- LeetCode/486. Predict the Winner.cpp
+++ LeetCode/486. Predict the Winner.cpp
@@ -26,12 +26,12 @@ class Solution {
 public:
     bool PredictTheWinner(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n));
+        vector<vector<int>> dp(n, vector<int>(n));   //dp为当子数组[i,j]时，Player1和Player2得到的分数之差
         for(int i = 0; i < n; i++)
             dp[i][i] = nums[i];
         for(int i = 1; i < n; i++)
             for(int j = 0; j+i < n; j++)
                 dp[j][j+i] = max(nums[j+i] - dp[j][j+i-1],nums[j] - dp[j+1][j+i]);
-        return dp[0][n-1] >= 0;
+        return dp[0][n-1] >= 0;     //分数差大于等于0，则Player1胜出
     }
 };
