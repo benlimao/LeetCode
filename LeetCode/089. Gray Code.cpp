3bae05f0c7a2b70b028ed7fd81a038be5cd98b02
benlimao <415639957@qq.com>
2017-08-24 21:52:16 +0800

    Create 089. Gray Code.cpp

---
+++ LeetCode/089. Gray Code.cpp
@@ -0,0 +1,36 @@
+//1.
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        vector<int> res;
+        if (n > 0)
+            grayCode(n, res);
+        else
+            res.push_back(0);
+        return res;
+    }
+    void grayCode(int n, vector<int>& res) {
+        if (n == 1) {
+            res.push_back(0);
+            res.push_back(1);
+            return;
+        }
+        grayCode(n-1, res);
+        int size = res.size()-1;
+        for (int i = size; i >= 0; --i) {
+            int num = res[i] | (1<<(n-1));
+            res.push_back(num);
+        }
+    }
+};
+
+//2.
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        vector<int> res(1<<n);
+        for (int i=0; i<(1<<n); i++) 
+            res[i] = i^(i>>1);
+        return res;
+    }
+};
