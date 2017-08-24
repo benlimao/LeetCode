7dc8ccd2dfc1d4e32fcd370a6d244fe6ab1037ce
benlimao <415639957@qq.com>
2017-08-24 16:17:30 +0800

    Create 034. Search for a Range.cpp

---
+++ LeetCode/034. Search for a Range.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> res(2, -1);
+        int n = nums.size();
+        if(n == 0)
+            return res;
+        int i = 0, j = n-1;
+        while (i < j)
+        {
+            int mid = (i + j) /2;
+            if (nums[mid] < target) 
+                i = mid + 1;
+            else 
+                j = mid;
+        }
+        if (nums[i]!=target) 
+            return res;
+        else 
+            res[0] = i;
+        j = n-1;  
+        while (i < j)
+        {
+            int mid = (i + j) /2 + 1;	
+            if (nums[mid] > target) 
+                j = mid - 1;  
+            else 
+                i = mid;			
+        }
+        res[1] = j;
+        return res; 
+    }
+};
