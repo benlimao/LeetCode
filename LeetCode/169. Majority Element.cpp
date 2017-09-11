489600582924afc6fd36cd6a44a34715eb317b0d
benlimao <415639957@qq.com>
2017-09-11 23:01:26 +0800

    Update 169. Majority Element.cpp

--- LeetCode/169. Majority Element.cpp
+++ LeetCode/169. Majority Element.cpp
@@ -1,3 +1,4 @@
+//1
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
@@ -25,4 +26,53 @@ public:
         }
         return res;
     }
+};
+
+//2
+class Solution {
+public:
+	int majorityElement(vector<int>& nums) {
+		int n = nums.size();
+		if (n == 0)
+			return -1;
+		if (n == 1)
+			return nums[0];
+		int mid = n >> 1;
+		int start = 0;
+		int end = n - 1;
+		int pos = partition(nums, start, end);
+		while (pos != mid)
+		{
+			if (pos > mid)
+			{
+				end = pos - 1;
+				pos = partition(nums, start, end);
+			}
+			else
+			{
+				start = pos + 1;
+				pos = partition(nums, start, end);
+			}
+		}
+		return nums[mid];
+	}
+
+	int partition(vector<int>& nums, int start, int end) {
+		//if (start > end)
+			//return -1;
+		int left = start;
+		int right = end;
+		int key = nums[start];
+		while (left < right)
+		{
+			while (left < right && nums[right] >= key)
+				right--;
+			while (left < right && nums[left] <= key)
+				left++;
+			if (left < right)
+				swap(nums[left], nums[right]);
+		}
+		swap(nums[start], nums[left]);
+		return left;
+	}
 };
